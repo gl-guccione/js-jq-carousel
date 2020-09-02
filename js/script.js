c3a040ee8ffe9a72d245ddb44f99b31b6b564334
gl-guccione <g.guccione3@gmail.com>
2020-09-02 16:24:57 +0200

    click on bullet lead to the correct image

--- js/script.js
+++ js/script.js
@@ -32,6 +32,28 @@ function prevImg() {
 }
 
 
+function bulletSelect() {
+  $(".bullet").removeClass("active");
+  $(this).addClass("active");
+
+  $("img.active").removeClass("active");
+
+  if ($(this).hasClass("bullet-01")) {
+    $("[src='img/img_0.jpg']").addClass("active");
+  } else if ($(this).hasClass("bullet-02")) {
+    $("[src='img/img_1.jpg']").addClass("active");
+  } else if ($(this).hasClass("bullet-03")) {
+    $("[src='img/img_2.jpg']").addClass("active");
+  } else if ($(this).hasClass("bullet-04")) {
+    $("[src='img/img_3.jpg']").addClass("active");
+  } else if ($(this).hasClass("bullet-05")) {
+    $("[src='img/img_4.jpg']").addClass("active");
+  } else if ($(this).hasClass("bullet-06")) {
+    $("[src='img/img_5.jpg']").addClass("active");
+  }
+}
+
+
 $("#next-button").click(nextImg);
 $(document).keydown(function(e){
   if (e.keyCode == 39)
@@ -44,3 +66,6 @@ $(document).keydown(function(e){
   if (e.keyCode == 37)
   prevImg();
 });
+
+
+$(".bullet").click(bulletSelect);
